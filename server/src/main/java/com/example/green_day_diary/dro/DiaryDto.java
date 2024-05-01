a3291aa8f01aa2b415838d720f97f9081e4a0129
Chaeyeon-Baek <163715888+Chaeyeon0@users.noreply.github.com>
2024-05-02 02:50:49 +0900

    [feature]DB 속성 이름 바꿈

--- server/src/main/java/com/example/green_day_diary/dro/DiaryDto.java
+++ server/src/main/java/com/example/green_day_diary/dro/DiaryDto.java
@@ -11,14 +11,14 @@ public class DiaryDto {
     private Long diary_id;
     private String diary_content;
     private LocalDate regdate;
-    private String user_email;
+    private String login_id;
 
     @Builder // 일기 DTO의 빌더 메서드
-    public DiaryDto(Long diary_id,String diary_content, LocalDate regdate, String user_email) {
+    public DiaryDto(Long diary_id,String diary_content, LocalDate regdate, String login_id) {
         this.diary_id = diary_id;
         this.diary_content = diary_content;
         this.regdate = regdate;
-        this.user_email = user_email;
+        this.login_id = login_id;
     }
 
     public DiaryEntity toEntity(){  // DTO를 엔티티로 변환하는 메서드
@@ -26,7 +26,7 @@ public class DiaryDto {
                 .diary_content(diary_content)
                 .regdate(regdate)
                 .diary_id(diary_id)
-                .user_email(user_email)
+                .login_id(login_id)
                 .build();
     }
 }
