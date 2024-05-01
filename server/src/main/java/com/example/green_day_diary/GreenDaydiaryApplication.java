01f36d4694fb9d493837650e6d629c5d201d1ab7
Chaeyeon-Baek <163715888+Chaeyeon0@users.noreply.github.com>
2024-05-01 23:13:58 +0900

    feature/#10/diary/commit

---
+++ server/src/main/java/com/example/green_day_diary/GreenDaydiaryApplication.java
@@ -0,0 +1,14 @@
+package com.example.green_day_diary;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class GreenDaydiaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenDaydiaryApplication.class, args);
+    }
+
+}
