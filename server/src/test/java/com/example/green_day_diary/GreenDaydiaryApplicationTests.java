443761eb98851f2f5bd591e7ec623477d3a4321c
Chaeyeon Baek <qorcodus03@naver.com>
2024-05-23 19:23:46 +0900

    [feature] test 수정

--- server/src/test/java/com/example/green_day_diary/GreenDaydiaryApplicationTests.java
+++ server/src/test/java/com/example/green_day_diary/GreenDaydiaryApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.green_day_diary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 class GreenDaydiaryApplicationTests {
 
     @Test
