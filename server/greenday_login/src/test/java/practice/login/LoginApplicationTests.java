d5562454318c2c5df8896ece793152dbe4f50c81
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-05-24 14:50:57 +0900

    [feature] 소셜로그인 배포관련 코드 수정

--- server/greenday_login/src/test/java/practice/login/LoginApplicationTests.java
+++ server/greenday_login/src/test/java/practice/login/LoginApplicationTests.java
@@ -3,7 +3,7 @@ package practice.login;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class LoginApplicationTests {
 
 	@Test
