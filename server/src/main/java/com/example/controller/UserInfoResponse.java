55c4dc75d123abae886cb932ecb16e872f16d125
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-06-02 21:55:16 +0900

    inforesponse 수정

--- server/src/main/java/com/example/controller/UserInfoResponse.java
+++ server/src/main/java/com/example/controller/UserInfoResponse.java
@@ -1,14 +1,12 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class UserInfoResponse {
     private String email;
     private String name;
     private String accessToken;
 
-    public UserInfoResponse(String email, String name) {
+    public UserInfoResponse(String email, String name, String accessToken) {
         this.email = email;
         this.name = name;
         this.accessToken = accessToken;
