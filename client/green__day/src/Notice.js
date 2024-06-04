5a5ebf4f84b732c6b17f6142e97d632ff90b0de9
aranlll <163715414+aranlll@users.noreply.github.com>
2024-06-04 19:33:24 +0900

    [feature]프론트 코드 수정

--- client/green__day/src/Notice.js
+++ client/green__day/src/Notice.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import Modal from "./modiary.js";
 import Home from "./Home.js";
 import History from "./history.js";
-
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import Pagination from "react-js-pagination";
+import "./App.css";
 
 function Notice() {
   let [text, setText] = useState("");
@@ -34,6 +33,11 @@ function Notice() {
   let [userInformation, setUserInformation] = useState([""]);
 
   useEffect(() => {
+    const userInfoFromStorage = localStorage.getItem("userInfo");
+    if (userInfoFromStorage) {
+      setUserInformation(JSON.parse(userInfoFromStorage));
+      console.log(userInfoFromStorage); // 유저 정보를 콘솔에 출력
+    }
     getBoardList();
   }, []);
 
@@ -43,6 +47,29 @@ function Notice() {
     ).data;
     setUserInformation(data);
     console.log(userInformation);
+  };
+  const navigate = useNavigate();
+
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState(2);
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
+  const itemChange = (e) => {
+    setItems(Number(e.target.value));
+  };
+
+  // 입력 데이터를 서버로 전송하는 함수
+  const sendDataToServer = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://codingapple1.github.io/shop/data2.json",
+        data
+      );
+      console.log("성공:", response.data);
+    } catch (error) {
+      console.error("실패:", error);
+    }
   };
 
   return (
@@ -77,23 +104,8 @@ function Notice() {
         <Modal isOpen={isModalOpen} onClose={closeModal} />{" "}
         {/* 모달을 닫기 위한 콜백 전달 */}
       </div>
-      {userInformation.map((a, i) => (
-        <div>
-          <div className="line1" />
-          <div className="userdata">
-            <div className="bar">
-              <div className="title">{a.title}</div>
-              <div className="writetime">Price:{a.price}</div>
-            </div>
-            <div className="noticeContent">{a.content}</div>
-            <br />
-            <br />
-            <br />
-          </div>
-        </div>
-      ))}
       <div className="input_data_list">
-        <div className="input1">{oldText}</div>
+        {/*         <div className="input1">{oldText}</div> */}
         <textarea
           className="input"
           placeholder="내용을 입력하세요"
@@ -110,13 +122,49 @@ function Notice() {
           >
             <img
               src="backrock_button.png"
+              alt="backrock button"
               onClick={() => {
                 setOldText({ text });
                 setOldText(text);
+
+                // 서버로 데이터 전송
+                sendDataToServer({ text });
               }}
             />
           </button>
         </div>
+
+        {userInformation
+          .slice(items * (page - 1), items * (page - 1) + items)
+          .map((a, i) => {
+            return (
+              <div key={i}>
+                <div className="line1" />
+                <div className="userdata">
+                  <div className="bar">
+                    <div className="title">{a.title}</div>
+                    <div className="writetime">Price:{a.price}</div>
+                  </div>
+                  <div className="noticeContent">{a.content}</div>
+                  <br />
+                  <br />
+                  <br />
+                </div>
+              </div>
+            );
+          })}
+        <>
+          <Pagination
+            className="pagination"
+            activePage={page}
+            itemsCountPerPage={items}
+            totalItemsCount={userInformation.length}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+          ></Pagination>
+        </>
+        {/* npm install react-js-pagination
+        yarn add react-js-pagination */}
       </div>
     </div>
   );
